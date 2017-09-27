2450ff08596d424462dbc0f2ac1e4b61f5d0a09a
Seif <seif.dorani@gmail.com>
2017-09-27 15:34:49 -0700

    scope

---
+++ JS/scope.js
@@ -0,0 +1,12 @@
+function doMath(){
+  var x = 40;
+  console.log(x);
+}
+doMath();
+//40 is outputed
+//however, if we just try to call x
+  // we will get undefined
+  // x is not defined
+  // x was declared locally within the function scope doMath
+  // and not globally outside the function
+  
