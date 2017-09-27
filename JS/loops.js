3b6257fa0923bc37a877a5bc0a8652c681649e42
Seif <seif.dorani@gmail.com>
2017-09-27 14:40:34 -0700

    another for loop ie, declare i make it 0, condition, when the loop should keep running, and last part of loop is our step, what do we do when condition is met

--- JS/loops.js
+++ JS/loops.js
@@ -5,7 +5,19 @@ for(init, condition, step){
 //understanding for loops
 
 //ie:
-//print numbers 1 to 5
+  //print numbers 1 to 5
 for (var count = 0; count < 6; count++){
   console.log(count);
 }
+
+//ie:
+  //print every char of a string
+
+var str = "hello";
+for(var i = 0; i < str.lenght; i++){
+  console.log(str[i]);
+}
+//no count var needed outside the loops
+  //create a temp val called i = 0
+  //everytime in the loop we add 1
+  // we keep going while count is less than the length of the word
