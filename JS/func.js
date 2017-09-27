d4d964bba0ab2ba78cafad52336e1b6393e51de5
Seif <seif.dorani@gmail.com>
2017-09-27 15:19:45 -0700

    importance of passing arguments

--- JS/func.js
+++ JS/func.js
@@ -3,3 +3,14 @@ function area(lenght, width){
 }
 area(2,4);//8
 //expcts an argument
+//arguments are useful
+ //creates dry code
+ //and allows us to change code
+
+
+//ie:
+  //score to increment or decrement
+  //user login, like checking credentials that take 2 arguments, (email,password);
+  //check them, using conditionals
+  //make a decision
+  //ability for functions to take arguments is so important: repeating code that we can channngeee
