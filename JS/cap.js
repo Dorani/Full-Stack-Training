35258e32c611f166eac374194eb41741c1d9f9d2
Seif <seif.dorani@gmail.com>
2017-09-27 15:30:36 -0700

    expression functions

--- JS/cap.js
+++ JS/cap.js
@@ -7,3 +7,18 @@ var city = "paris"
 var capital = capitalize(city);
 
 //we can capture the returned value in a variable
+
+
+//another way
+//function expression
+
+//create a var
+var capitalize = function(str){
+  return str.chatAt(0).toUpperCase() + str.slice(1);
+}
+
+// however:
+  // the diff is if i decide to change capitalize to be = to a diff data type
+  // my func will be lost
+
+  
