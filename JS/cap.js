7d61dd968fff6097162db63d80c67e624acbb151
Seif <seif.dorani@gmail.com>
2017-09-27 15:27:43 -0700

    comments on logic of func

--- JS/cap.js
+++ JS/cap.js
@@ -1,5 +1,6 @@
 function capitalize(str){
   return str.chatAt(0).toUpperCase() + str.slice(1);
+  //return str 1st characted at index 0 to uppercase method, then combining everything after the letter p with capital P
 }
 
 var city = "paris"
