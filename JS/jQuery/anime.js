d3331dcc13636f0c3ebc0cedd2bc6024f2839a8e
Seif <seif.dorani@gmail.com>
2017-10-03 11:14:06 -0700

    selecting in Jquery

---
+++ JS/jQuery/anime.js
@@ -0,0 +1,15 @@
+//so many different ways of selecting in js
+//jQuery makes this easier
+
+//to select all img tags
+$('img')
+
+//to select all elemtns with class 'sale'
+$(".sale")
+
+//to select element with id 'bonus'
+
+$("#bonus")
+
+//to select all a tags inside of li's
+$("li a")
