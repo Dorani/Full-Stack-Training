3058f120fe1612f2f8e7d9ef0c7a30ecb18a8f99
Seif <seif.dorani@gmail.com>
2017-10-04 14:12:34 -0700

    create obj of props to take affect if condition is met

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -3,7 +3,10 @@ $("li").click(function(){
   //if li is gray
   if($(this).css("color") === "rgb(128, 128, 128)"){
       // turn it black
-      $(this).css("color", "black");
+      $(this).css(
+        {color: "black",
+        textDecoration: "none"
+      });
 
   }
     // else turn it grey
