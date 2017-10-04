aa0a432da17fd1c9bb59d5e7c4460d7a5b9061e3
Seif <seif.dorani@gmail.com>
2017-10-04 14:14:55 -0700

    ok so created a class that will have the affected change take place, doing this to redeuce all that code into one line, the power of jquery

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -3,19 +3,16 @@ $("li").click(function(){
   //if li is gray
   if($(this).css("color") === "rgb(128, 128, 128)"){
       // turn it black
-      $(this).css(
-        {color: "black",
+      $(this).css({
+        color: "black",
         textDecoration: "none"
       });
-
-  }
+    }
     // else turn it grey
-  else {
+    else {
     $(this).css({
       color: "gray",
       textDecoration: "line-through"
     });
   }
-
-
 });
