d0eb86cc11d650bf6215d1f2c54d057df05c366c
Seif <seif.dorani@gmail.com>
2017-10-04 10:43:21 -0700

    quick demo of events in jquery

---
+++ JS/jQuery/events/events.js
@@ -0,0 +1,20 @@
+//click()
+//keypress()
+//on() used 99% of the times
+
+
+
+
+
+//-----------------------------------
+
+//print when item with id 'submit is clicked
+
+$("#submit").click(function(){
+  console.log("another click");
+});
+
+//alerts when ANY buttin is clicked
+$("button").click(function(){
+  alert("somone clicked button");
+});
