e0685ef2277bbac1fcd23333756f150303fb5445
Seif <seif.dorani@gmail.com>
2017-10-04 14:19:01 -0700

    create class with props in css and used jquery method toggle class to ammend the li on click

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,18 +1,24 @@
 //check off specific todos by clicking
+// $("li").click(function(){
+//   //if li is gray
+//   if($(this).css("color") === "rgb(128, 128, 128)"){
+//       // turn it black
+//       $(this).css({
+//         color: "black",
+//         textDecoration: "none"
+//       });
+//     }
+//     // else turn it grey
+//     else {
+//     $(this).css({
+//       color: "gray",
+//       textDecoration: "line-through"
+//     });
+//   }
+// });
+
+//above code in 1 line:
+
 $("li").click(function(){
-  //if li is gray
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-      // turn it black
-      $(this).css({
-        color: "black",
-        textDecoration: "none"
-      });
-    }
-    // else turn it grey
-    else {
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
-  }
+  $(this).toggleClass("completed");
 });
