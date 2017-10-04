969670fdd710e99dfb4ac70b1c6460f376a29a50
Seif <seif.dorani@gmail.com>
2017-10-04 11:36:42 -0700

    more on fading

--- JS/jQuery/effects/effects.js
+++ JS/jQuery/effects/effects.js
@@ -6,5 +6,35 @@ $("divs").fadeOut():
 
 //now do the same when its clicked
 $("button").on("click", function(){
-  $("divs").fadeOut();
+  $("divs").fadeOut(1000);//adding time with the argument passed in milli seconds
+});
+
+//print a message on the divs faded
+$("button").on("click", function(){
+  $("divs").fadeOut(1000);//adding time with the argument passed in milli seconds
+  console.log("face completed");
+});
+
+//this takes a full second so we should add a callback auto when done
+$("button").on("click", function(){
+  $("divs").fadeOut(1000, function(){
+    console.log("face completed");
+  });
+});
+//fade out finished then 3 console.logs
+
+
+
+//now lets have them delete completely when faded out
+$("button").on("click", function(){
+  $("divs").fadeOut(1000, function(){
+    $(this).remove();// now our body should only have a button and no longer displaying the divs, set to none
+  });
+});
+
+//same can happen with fadeIn
+$("button").on("click", function(){
+  $("divs").fadeIn(1000, function(){
+    console.log("divs are back");
+  });
 });
