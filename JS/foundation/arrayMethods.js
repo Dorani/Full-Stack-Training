4920c9d2435a37af802cf58b5b0aa4223ee6f51c
Seif <seif.dorani@gmail.com>
2017-10-07 23:51:48 -0700

    more on js methods

--- JS/foundation/arrayMethods.js
+++ JS/foundation/arrayMethods.js
@@ -3,3 +3,11 @@
 var  array = ['banana', 'orange'];
 array.push('berries');//add berries to array list
 array.pop();//remove berries, last element from list
+
+var first = fruits[0];
+// banana
+
+var last = fruits[fruits.length - 1];
+// orange
+
+array.unshift('lemon');//add lemon to the top of list, the 0 index.
