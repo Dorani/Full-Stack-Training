954a2a4413829339d49678518eb0554ab8a845d3
Seif <seif.dorani@gmail.com>
2017-10-09 16:32:47 -0700

    forEach method comparison

--- JS/foundation/for.js
+++ JS/foundation/for.js
@@ -11,3 +11,22 @@ var color = ['red','green','orange'];//create an array and assign it to color
 for(var i = 0; i < color.length; i++){//i starts at 0, then we add 1 to i, and we keep going while i < the length of the color array
   console.log(color[i]);//log each index of the array
 }
+
+//forEach
+  //new loop in js
+  //came out in 2009
+  //best way to loop through array
+  //we pass a function into another function
+
+
+//method thats defined on every single array
+//part of the array prototype, where all those cool methods live
+  //takes a function as an argument
+  //pass in a function
+  //then call it
+
+
+var colors = ['red','green','orange'];//create an array and assign it to color
+colors.forEach(function(color){
+  console.log(color);
+});
