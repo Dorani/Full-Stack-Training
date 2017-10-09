874d68a717afc71a1c2151f3aff110030c037336
Seif <seif.dorani@gmail.com>
2017-10-09 16:55:06 -0700

    intro to bootstrap

---
+++ Bootstrap/boot.js
@@ -0,0 +1,20 @@
+//getboostrap.com
+//what is bootstrap and why use it?
+  //popular html+css+js framework for building awesome responsive web apps
+  //single file of css and js
+  //a lot of both but they are 2 files that is included into our own apps
+    //helps us make sick sites that are responsive and fast
+    //widely used and worth knowing
+    //helps making good looking sites quickly so we can focus on the back-end!
+
+
+
+//great docs
+  //click on css
+  //we get awesome tools for free
+  //buttons
+  //forms etc
+
+//components:
+  // nav bars
+  // really cool stuff!
