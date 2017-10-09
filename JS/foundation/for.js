0df3fb5aad1989894ac8b99439229e8e79eee5ab
Seif <seif.dorani@gmail.com>
2017-10-09 16:27:09 -0700

    for loop example

--- JS/foundation/for.js
+++ JS/foundation/for.js
@@ -3,3 +3,11 @@
   //those comments are stored in an array
   //in order to display comments
   //a loop is used to go itterate through array to print out each comments
+
+//for loop
+//loop through array
+//print out each orange
+var color = ['red','green','orange'];//create an array and assign it to color
+for(var i = 0; i < color.length; i++){//i starts at 0, then we add 1 to i, and we keep going while i < the length of the color array
+  console.log(color[i]);//log each index of the array
+}
