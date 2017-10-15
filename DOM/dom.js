3560932fd45647898f11bc82149ea620442ec070
Seif <seif.dorani@gmail.com>
2017-10-15 00:19:50 -0700

    dom basics

---
+++ DOM/dom.js
@@ -0,0 +1,19 @@
+//DOM:
+  // Document object model
+    // interface between js and html and compress
+    // cool ways to interact
+
+
+// The browser turns every html tag into a js object
+  // load html in browser
+  // the dom is created from above
+  // it essentially turns every element into js objects
+  // behind the scenes it makes a model of every js object
+
+//Document:
+
+//in the console:
+// console.dir(document)
+  // this another method that will return the entire document object
+  // a regular object
+    // with a lot of info like fonts, imgs,links, body with props
