01ffd455c943ac09685831ab2e51991135a23e49
Seif <seif.dorani@gmail.com>
2017-10-15 00:29:53 -0700

    dom manipulation ie

--- DOM/dom.js
+++ DOM/dom.js
@@ -10,10 +10,30 @@
   // it essentially turns every element into js objects
   // behind the scenes it makes a model of every js object
 
-//Document:
+//Document: The root node where everything lives
 
 //in the console:
 // console.dir(document)
   // this another method that will return the entire document object
   // a regular object
     // with a lot of info like fonts, imgs,links, body with props
+    // great way to investigate!s
+
+//We will write js code that will select elements that will return it back
+//and change properties/manipulate
+
+//so let's select the h1 in the dom.html
+var h1 = document.querySelector('h1');
+//returns a obj that represents this h1
+//saves it in h1 var
+//selectin process
+h1.style.color = 'pink';
+//manipulating
+//change color, h1 var that has the obj represting the h1,
+//.style 1 property, that is a huge obj with a bunch of other properties
+//color is one of them, browser sees that and updates
+
+//select + manipulating above
+
+
+//
