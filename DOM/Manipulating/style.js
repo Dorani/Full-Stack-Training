cb5d44c19ae4c9420e2113857dab1c95152e4cfc
Seif <seif.dorani@gmail.com>
2017-10-16 11:20:06 -0700

    important note on manipulating

--- DOM/Manipulating/style.js
+++ DOM/Manipulating/style.js
@@ -7,3 +7,6 @@ tag.style.border = "10px solid red";
 tag.style.fontSize = "70px";
 tag.style.background = "yellow";
 tag.style.marginTop = "200px";
+//using style property
+//even if in css we do not need quotes on the other side stating the property
+//its important to remember, this is NOT CSS, this is JS with css properties
