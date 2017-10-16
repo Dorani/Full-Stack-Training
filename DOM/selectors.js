e44dbbbe987acd0937a6c755944a72d2f884e5b9
Seif <seif.dorani@gmail.com>
2017-10-15 20:23:25 -0700

    first selector method in action, saved to a var called tag

--- DOM/selectors.js
+++ DOM/selectors.js
@@ -1,6 +1,12 @@
 //Diff ways of selecting elements:
 
 document.getElementById();
+//takes in id name, returns element with that id
+//example with selector.html
+  var tag = document.getElementById('highlight');
+
+
+
 
 document.getElementByClassName();
 
