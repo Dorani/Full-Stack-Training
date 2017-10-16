447a674975ff193225d41af7c21bef0d32d269e7
Seif <seif.dorani@gmail.com>
2017-10-15 20:25:11 -0700

    byID

--- DOM/selectors.js
+++ DOM/selectors.js
@@ -4,11 +4,16 @@ document.getElementById();
 //takes in id name, returns element with that id
 //example with selector.html
   var tag = document.getElementById('highlight');
+//call the method, pass in a single string argument, then it finds a matching element with that id and returns it
 
 
 
 
 document.getElementByClassName();
+//
+
+
+
 
 document.getElementByTagName();
 
