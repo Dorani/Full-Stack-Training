c4048689908f365b85f2fd515b1d4adbc6cd64a8
Seif <seif.dorani@gmail.com>
2017-10-16 12:04:17 -0700

    more on manipu

--- DOM/googleManipulation.js
+++ DOM/googleManipulation.js
@@ -4,12 +4,11 @@
 //isolate the id
 //assign it to a var
 //and begin manipulating!
-
 var logo = document.querySelector("#hplogo");
 
 
-
-
 //change source
-
 logo.setAttribute('src', 'http://www.facebook.com');
+
+//changing style
+logo.style.width = '500px';
