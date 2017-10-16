5bea327ed4096d0a3c491d763674403f37023a3e
Seif <seif.dorani@gmail.com>
2017-10-16 12:10:33 -0700

    more manipualting

--- DOM/googleManipulation.js
+++ DOM/googleManipulation.js
@@ -12,3 +12,32 @@ logo.setAttribute('src', 'http://www.facebook.com');
 
 //changing style
 logo.style.width = '500px';
+logo.style.height = '1000px';
+logo.style.border = '2px solid purple';
+
+
+
+
+
+//selecting a bunch of elements in a tag
+var links = document.getElementsByTagName('a');
+//loop through them
+for(var i = 0; i<links.length; i++){
+  console.log(links[i].textContent);
+}
+//display content
+//gmail, images, sign in, etc.. all the links!
+
+
+for(var i = 0; i<links.length; i++){
+  links[i].style.background = 'pink';
+}
+//let's change the style with that loop
+//now all the links on the page are pink
+
+
+for(var i = 0; i<links.length; i++){
+  links[i].style.border = '1px dashed purple';
+  links[i].style.color = 'orange';
+}
+//more style changes on all links
