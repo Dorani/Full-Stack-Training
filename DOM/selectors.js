bb443502b3678d99caa083a74f03bf6d3e16b1d1
Seif <seif.dorani@gmail.com>
2017-10-15 23:57:59 -0700

    more on selectors

--- DOM/selectors.js
+++ DOM/selectors.js
@@ -1,4 +1,5 @@
 //Diff ways of selecting elements:
+//Using selector.html
 
 document.getElementById();
 //takes in id name, returns element with that id
@@ -14,11 +15,19 @@ document.getElementsByClassName();
 //class can occur as many times which is why its plural
 //in action
 var tags = document.getElementsByClassName('bolded');
-//tags will return 2 obj with bolded props
+//tags will return 2 obj with tons of properties
+//list of 2 lis
+//node list which is array like
+//we can access elements etc.. but no forEach loop
 
 
+document.getElementsByTagName();
+//returns a list of elements that match a tag name, like li, or h1, body etc
+//in this case
+//we should get a list of the 3 lis that live ul
+//objects
+var tags = document.getElementsByTagName('li');
 
-document.getElementByTagName();
 
 document.querySelector();
 
