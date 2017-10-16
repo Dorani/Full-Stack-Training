c57c0983c58ffb7bfa723f6b7e6e30d900890dc3
Seif <seif.dorani@gmail.com>
2017-10-16 11:24:13 -0700

    more notes on seperation of concerns

--- DOM/Manipulating/style.js
+++ DOM/Manipulating/style.js
@@ -10,3 +10,10 @@ tag.style.marginTop = "200px";
 //using style property
 //even if in css we do not need quotes on the other side stating the property
 //its important to remember, this is NOT CSS, this is JS with css properties
+
+
+//seperation of concerns
+//html css and js are each responsible for their domains
+//structure, behavior and presentation
+//html is pure structure and css is pure style
+//js controls behavior
