5a754677b2e7d50bbcf150b0e2a983d2ecf5e5a9
Seif <seif.dorani@gmail.com>
2017-10-15 20:17:03 -0700

    selectors we will be analyznu

---
+++ DOM/selectors.js
@@ -0,0 +1,11 @@
+//Diff ways of selecting elements:
+
+document.getElementById();
+
+document.getElementByClassName();
+
+document.getElementByTagName();
+
+document.querySelector();
+
+document.querySelectorAll();
