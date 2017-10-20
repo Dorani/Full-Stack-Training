fb14ee4e5256e73728ff96af9eea60e8ff0dd5ed
Seif <seif.dorani@gmail.com>
2017-10-20 11:07:35 -0700

    created cool effects when clicked on with some css props and click event

--- DOM/DOM advanced/hover.js
+++ DOM/DOM advanced/hover.js
@@ -9,4 +9,7 @@ for(var i = 0; i < lis.length; i++){
   lis[i].addEventListener('mouseout', function(){
     this.style.color = "black";
   });
+  lis[i].addEventListener('click', function(){
+    this.classList.toggle('done');
+  });
 }
