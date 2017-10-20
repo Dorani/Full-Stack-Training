c117445f7ea5d87f93ba4630cfa78d0e0d45633d
Seif <seif.dorani@gmail.com>
2017-10-19 17:08:10 -0700

    see in console now, when we click each time score increments by 1, lets get it to append to where it says 0 to 0

--- DOM/Dom Project/score.js
+++ DOM/Dom Project/score.js
@@ -2,7 +2,12 @@
 //select player 2 button
 var p1Button = document.querySelector('#p1');
 var p2Button = document.querySelector('#p2');
+//we now need a way to keep track of score
+
+var p1Score = 0;
+
 //set up event handler, with callback and alert for now
 p1Button.addEventListener('click', function(){
-  alert('it works for now');
+  p1score++;
+  console.log(p1score);
 });
