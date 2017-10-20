79f56e31d9da0f0123ede582c0552fbad234f603
Seif <seif.dorani@gmail.com>
2017-10-20 11:11:21 -0700

    notes on seperation of concerns, and ie

--- DOM/DOM advanced/hover.js
+++ DOM/DOM advanced/hover.js
@@ -4,10 +4,10 @@ var lis = document.querySelectorAll('li');
 
 for(var i = 0; i < lis.length; i++){
   lis[i].addEventListener('mouseover', function(){
-    this.style.color = "green";
+    this.classList.add('selected');//seperation of concerns, using classes always best practice, css will manipulate style and js witll trigger css
   });
   lis[i].addEventListener('mouseout', function(){
-    this.style.color = "black";
+      this.classList.remove('selected');//seperation of concerns
   });
   lis[i].addEventListener('click', function(){
     this.classList.toggle('done');
