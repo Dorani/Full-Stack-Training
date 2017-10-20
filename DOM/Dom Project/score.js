a99de9f9843ea16a76b06e02138cacc8cf430852
Seif <seif.dorani@gmail.com>
2017-10-19 17:18:04 -0700

    iso, and appended changes to p1Display

--- DOM/Dom Project/score.js
+++ DOM/Dom Project/score.js
@@ -2,13 +2,14 @@
 //select player 2 button
 var p1Button = document.querySelector('#p1');
 var p2Button = document.querySelector('#p2');
+var p1Display = document.querySelector('#p1Display');
 //we now need a way to keep track of score
 var p1Score = 0;
 //select the h1 to start
-var h1 = document.querySelector('h1');
+var h1 = document.querySelector('p1Display');
 
 //set up event handler, with callback and alert for now
 p1Button.addEventListener('click', function(){
   p1Score++;
-  h1.textContent = p1Score;//now take the h1 and change the context to be wtvr the score is
+  p1Display.textContent = p1Score;//now take the h1 and change the context to be wtvr the score is
 });
