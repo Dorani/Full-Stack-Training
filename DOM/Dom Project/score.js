1372a937583aa682f264403e97409084bfbab291
Seif <seif.dorani@gmail.com>
2017-10-19 17:11:19 -0700

    corrected some errors, works now

--- DOM/Dom Project/score.js
+++ DOM/Dom Project/score.js
@@ -3,11 +3,12 @@
 var p1Button = document.querySelector('#p1');
 var p2Button = document.querySelector('#p2');
 //we now need a way to keep track of score
-
 var p1Score = 0;
+//select the h1 to start
+var h1 = document.querySelector('h1');
 
 //set up event handler, with callback and alert for now
 p1Button.addEventListener('click', function(){
-  p1score++;
-  console.log(p1score);
+  p1Score++;
+  h1.textContent = p1Score;//now take the h1 and change the context to be wtvr the score is
 });
