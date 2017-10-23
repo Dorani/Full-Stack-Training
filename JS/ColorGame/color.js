93e6c3714690128d26766c9979a9e9be99cf8144
Seif <seif.dorani@gmail.com>
2017-10-22 17:36:46 -0700

    array of 6 colors as strings

--- JS/ColorGame/color.js
+++ JS/ColorGame/color.js
@@ -2,3 +2,17 @@
 //and we will be provided rgb color (222,333,4)
 //we then have to guess which color it is
 //the color will be provided below
+
+//assign 6 colors that are always the same
+
+
+//make a list of colors
+
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)",
+]
