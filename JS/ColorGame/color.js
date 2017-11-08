fbfb1a8162507a7953ca6da186dfff1ddd732024
Seif <seif.dorani@gmail.com>
2017-11-08 10:26:32 -0800

    refactoring, removing ids from easy and hard button elements, replacing them with class

--- JS/ColorGame/color.js
+++ JS/ColorGame/color.js
@@ -59,8 +59,9 @@ resetButton.addEventListener("click", function(){
   pickedColor = pickColor();
   //change color display to match picked color
   colorDisplay.textContent = pickedColor;
-  messageDisplay.textContent = "";
   this.textContent = "New Colors";
+  messageDisplay.textContent = "";
+
   //change the colors of the squares on the page
   for(var i = 0; i < squares.length; i++){
     squares[i].style.background = colors[i];
