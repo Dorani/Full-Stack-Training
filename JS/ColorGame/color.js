e22f65dc7dda5ec8074c9b6124f6c4acdb6c5228
Seif <seif.dorani@gmail.com>
2017-11-08 10:28:11 -0800

    now that both buttons have the same class, we create a var and isolate those buttons

--- JS/ColorGame/color.js
+++ JS/ColorGame/color.js
@@ -16,8 +16,7 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
 
 
