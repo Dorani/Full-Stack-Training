87804fc9d11be24063192fd28ff2abedc245756c
Seif <seif.dorani@gmail.com>
2018-01-03 17:46:01 -0800

    more notes on routes

--- Back-End/test.js
+++ Back-End/test.js
@@ -10,10 +10,13 @@ app.get("/", function(req, res){
 
 app.get("/dogs", function(req, res){
   dog.find({}, function(err, dogs){
-    res.render("dogs",{dogs: dogs});
+    res.render("dogs",{dogs: dogs}); //rendering a page/template called dogs,
+    //but we are passing data to this template
+    //so the ejs can be dynamic and take in all diff kinds of database
+    // for every dog in the database that we retrieve we will loop through and make a single li with the name and breed
   });
 })
 // lsning for a get request to / dogs
 // retrrives all of the dogs from database
 // rendering the dogs.ejs file
-// 
+//
