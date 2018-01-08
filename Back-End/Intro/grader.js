1f040571ff6699bd9b8ae730a6e4a7089236c9be
Seif <seif.dorani@gmail.com>
2018-01-07 23:34:17 -0800

    explanation

--- Back-End/Intro/grader.js
+++ Back-End/Intro/grader.js
@@ -13,3 +13,11 @@ function average(scores){
 
 var scores = [90, 98, 89, 100, 100, 86, 94];
 console.log(average(scores));
+
+//pass in array
+//called scores
+//start a total var at 0
+//loop through scores, using forEach
+//add each score into total
+//devide total by all the scores, and set it to average
+//then math.round avg and return that
