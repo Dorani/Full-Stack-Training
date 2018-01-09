17ddc98b4ed40d38fc593b58944e0322c06eb05f
Seif <seif.dorani@gmail.com>
2018-01-08 22:45:29 -0800

    another example, with comments

--- Back-End/MyShop/listProduct.js
+++ Back-End/MyShop/listProduct.js
@@ -1,5 +1,6 @@
 var faker = require("faker");
 
-var randomName = faker.name.findName();
-
+var randomName = faker.name.findName();//faker.category.method
+var randomEmail = faker.internet.email();//faker.category.method
 console.log(randomName);
+console.log(randomEmail);
