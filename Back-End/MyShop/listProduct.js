2b0449e6031c806d2564e786ecf23316e282a0de
Seif <seif.dorani@gmail.com>
2018-01-08 22:48:43 -0800

    generate random product name, make sure you read the faker npm docs and isolate the category you want and associated methods

--- Back-End/MyShop/listProduct.js
+++ Back-End/MyShop/listProduct.js
@@ -4,3 +4,11 @@ var randomName = faker.name.findName();//faker.category.method
 var randomEmail = faker.internet.email();//faker.category.method
 console.log(randomName);
 console.log(randomEmail);
+
+
+//check out docs
+  //go under api methods
+  //isolate commerce category
+    //productName and price methods
+
+console.log(faker.commerce.productName());
