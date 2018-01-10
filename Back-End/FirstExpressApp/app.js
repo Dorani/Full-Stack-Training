58e8b846817f47d1518a938786d3b62329155d3a
Seif <seif.dorani@gmail.com>
2018-01-09 23:20:10 -0800

    get request to the root path

--- Back-End/FirstExpressApp/app.js
+++ Back-End/FirstExpressApp/app.js
@@ -2,3 +2,19 @@ var express = require("express")
 //import express, includes all content of ex
 var app = express();
 //initialize variable called app and execute it
+
+
+// when we go to "/" we should see "hi there!"
+app.get("/", function(req, res){
+  res.send("hi there!");
+});
+
+
+// when we go to "/bye" we should see "goodbye!"
+
+
+
+// when we go to "/dog" we should see "meow!"
+
+
+//tell express to listen for request (start server)
