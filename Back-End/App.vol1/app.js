7c039a20ea86644a158a6f4530b355b618124489
Seif <seif.dorani@gmail.com>
2018-01-15 18:29:08 -0800

    route patern, output the route name with number of times as well

--- Back-End/App.vol1/app.js
+++ Back-End/App.vol1/app.js
@@ -29,10 +29,16 @@ app.get("/speak/:animal", function(req,res){
 
 app.get("/repeat/:message/:times", function(req,res){
   var message = req.params.message;
-  var times = req.params.times;
-  res.send("Message" + message + " Times" + times);
-})
+  var times = Number(req.params.times);
+  var result = "";
 
+  for(var i = 0; i < times; i++){
+    result += message + " ";
+  }
+  res.send(result);
+})
+//send back result string by going through the loop
+//hi/10 should get 10 his
 
 
 
