26aab7b07fb669c77783666972af83533674e58a
Seif <seif.dorani@gmail.com>
2018-01-15 18:09:47 -0800

    server created

--- Back-End/App.vol1/app.js
+++ Back-End/App.vol1/app.js
@@ -4,3 +4,14 @@ var app = express();
 app.get("/", function(req,res){
   res.send("hi there, welcome to my work")
 });
+
+
+
+
+
+//tell express to listen for request (start server)
+//we need to write the code to tell it to lsn to requests
+
+app.listen(3000, function(){
+  console.log("Serving demo app on port 3000");
+});
