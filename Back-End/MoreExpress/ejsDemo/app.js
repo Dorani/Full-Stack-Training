cdc69798e019c95f844b25d5a2c138bd29184572
Seif <seif.dorani@gmail.com>
2018-01-21 01:02:24 -0800

    launcing server

--- Back-End/MoreExpress/ejsDemo/app.js
+++ Back-End/MoreExpress/ejsDemo/app.js
@@ -2,3 +2,18 @@ var express = require("express");
 var app = express();
 //two steps usually, require 1st, then initialize var app make it
 //equal express executed as a function
+
+
+
+
+
+
+
+
+
+
+
+//launch server
+app.listen(3000, function(){
+  console.log("server is listening")
+});
