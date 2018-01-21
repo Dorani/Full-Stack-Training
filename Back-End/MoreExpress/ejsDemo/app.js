22fb69eb4f1659cbe42c363f54fea0624882ad4b
Seif <seif.dorani@gmail.com>
2018-01-21 01:04:02 -0800

    1st route, root, with msg

--- Back-End/MoreExpress/ejsDemo/app.js
+++ Back-End/MoreExpress/ejsDemo/app.js
@@ -3,7 +3,9 @@ var app = express();
 //two steps usually, require 1st, then initialize var app make it
 //equal express executed as a function
 
-
+app.get('/', function(req,res){
+  res.send("welcome to the home page");
+});
 
 
 
