ad2fffdc480a83ea492151d1299be46a27702d45
Seif <seif.dorani@gmail.com>
2018-03-10 22:39:46 -0800

    all set, basic skell setup, express and ejs included, set our default engine and our root route

--- Back-End/PostRequestDemo/app.js
+++ Back-End/PostRequestDemo/app.js
@@ -3,6 +3,13 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.listen(3000, function(){
-  console.log("server is up and running");
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+
+
+//launch server
+app.listen(8000, function(){
+  console.log("server is listening!!!!")
 });
