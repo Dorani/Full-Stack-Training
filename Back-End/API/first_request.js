92cfc57ca44a3239e00b9aefee65a35dfde110cd
Seif <seif.dorani@gmail.com>
2018-07-23 11:17:45 -0700

    cleaner way

--- Back-End/API/first_request.js
+++ Back-End/API/first_request.js
@@ -1,9 +1,12 @@
 var request = require('request');
 
-request('http://www.google.com', function(error, response, body){
-  if (!error && response.statusCode == 200) {
-    console.log(body)//show html for google page
+request('http://www.google.com', function (error, response, body){
+  if (error){
+    console.log('something went wrong');
+    console.log(error)
+  } else {
+    if (response.statusCode == 200){
+      console.log(body)
+    }
   }
-})
-
-request('http://www.google.com');
+});
