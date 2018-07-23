61121bf431843cb3588fbb6dc2693a0155fb35d6
Seif <seif.dorani@gmail.com>
2018-07-23 12:19:35 -0700

    returning data of 1s element of search arrauy from the response obj

--- Back-End/API/3rd API-OMDB/app.js
+++ Back-End/API/3rd API-OMDB/app.js
@@ -2,9 +2,18 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-
+//created routes called results
+//made an api request to omdbapi
+//parse data coming back
+//used parsedData container to isolate 1st element of array
+//send to results route
 app.get("/results", function(req,res){
-  res.send('hello it works');
+  request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response,body){
+    if (!error && response.statusCode == 200){
+      var parsedData = JSON.parse(body);
+      res.send(parsedData["Search"][0]);
+    }
+  });
 });
 
 
