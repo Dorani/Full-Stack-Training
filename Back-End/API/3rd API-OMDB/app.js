aa97acdba04c985383364d7b2822ecded6625b17
Seif <seif.dorani@gmail.com>
2018-07-23 12:27:44 -0700

    created ejs template and sent response there

--- Back-End/API/3rd API-OMDB/app.js
+++ Back-End/API/3rd API-OMDB/app.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
+
+//(TAKING OUR REQUEST CODE AND PUTING IT IN OUR APP:)
 //created routes called results
 //made an api request to omdbapi
 //parse data coming back
@@ -11,7 +15,7 @@ app.get("/results", function(req,res){
   request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response,body){
     if (!error && response.statusCode == 200){
       var parsedData = JSON.parse(body);
-      res.send(parsedData["Search"][0]);
+      res.render("results");
     }
   });
 });
