14afd92940d0ba39752c765dfb724457ab79462c
Seif <seif.dorani@gmail.com>
2018-07-23 12:01:38 -0700

    edits, and routes with soncole log to test

--- Back-End/API/3rd API-OMDB/app.js
+++ Back-End/API/3rd API-OMDB/app.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var ejs = require('ejs');
+var app = require('express');
+var app = express();
 var request = require('request');
 
 
+app.get("/results", function(req,res){
+  res.send('hello it works');
+});
+
+
+
+
+
 //tell express to listen for request (start server)
-//we need to write the code to tell it to lsn to requests
 app.listen(3000, function(){
   console.log("Movie app on port 3000");
 });
