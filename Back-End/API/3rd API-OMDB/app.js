832f995024cecd166c56f6bb1e16a8d74fa7ed94
Seif <seif.dorani@gmail.com>
2018-07-23 11:59:29 -0700

    tell express to lsn for app

--- Back-End/API/3rd API-OMDB/app.js
+++ Back-End/API/3rd API-OMDB/app.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var ejs = require('ejs');
 var request = require('request');
+
+
+//tell express to listen for request (start server)
+//we need to write the code to tell it to lsn to requests
+app.listen(3000, function(){
+  console.log("Movie app on port 3000");
+});
