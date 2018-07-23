8c1ad3515a1407800843499c7525c17a91a61540
Seif <seif.dorani@gmail.com>
2018-07-23 12:03:59 -0700

    typo in requiring modules

--- Back-End/API/3rd API-OMDB/app.js
+++ Back-End/API/3rd API-OMDB/app.js
@@ -1,6 +1,6 @@
-var app = require('express');
+var express = require("express");
 var app = express();
-var request = require('request');
+var request = require("request");
 
 
 app.get("/results", function(req,res){
