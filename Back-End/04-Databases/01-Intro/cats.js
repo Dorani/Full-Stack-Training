a19bae9d70eb2a463773923dabc5e2edeab9b7bf
Seif <seif.dorani@gmail.com>
2018-08-13 10:27:08 -0700

    steps

---
+++ Back-End/04-Databases/01-Intro/cats.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+//we need to connect to a database
+//run mongod = our daemon server
+//we need to tell mongoose to connect to this server we have running
+
+//adding a new cat to DB
+
+//retrieve all cats from DB and console.log each One
