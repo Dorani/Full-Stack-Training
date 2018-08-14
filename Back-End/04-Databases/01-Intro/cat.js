cfbb0cc097c4ee69d2c61916874c1662bd0a8982
Seif <seif.dorani@gmail.com>
2018-08-13 20:47:45 -0700

    edits

--- Back-End/04-Databases/01-Intro/cat.js
+++ Back-End/04-Databases/01-Intro/cat.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 //we need to connect to a database
 //run mongod = our daemon server
 //we need to tell mongoose to connect to this server we have running
-// mongoose.connect('mongodb://localhost:27017/cat.js', { useNewUrlParser: true });//url + default port number + name of DB
-// //if I do not have a cat app db, it will run it, see that and make One
+// mongoose.connect('mongodb://127.0.0.1:27017/cat_app', { useNewUrlParser: true });//url + default port number + name of DB
+// if I do not have a cat app db, it will run it, see that and make One
 
 const options = {
  useNewUrlParser: true
