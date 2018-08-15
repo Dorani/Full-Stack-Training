10d3d83088affb7ec2459d1d1fee434e03d047d7
Seif <seif.dorani@gmail.com>
2018-08-14 23:38:00 -0700

    edits

--- Back-End/04-Databases/01-Intro/cat.js
+++ Back-End/04-Databases/01-Intro/cat.js
@@ -37,20 +37,39 @@ var Cat = mongoose.model("Cat", catSchema);
           //adding a new cat to DB
 
 
-var george = new Cat({
-  name: "George",
-  age: 11,
-  temperment: "Grouchy"
-});
-//added to db
-george.save(function(err, cat){
-  if (err){
-    console.log("something went wrong")
+// var george = new Cat({
+//   name: "George",
+//   age: 11,
+//   temperment: "Grouchy"
+// });
+// //added to db
+// george.save(function(err, cat){
+//   if (err){
+//     console.log("something went wrong")
+//   } else {
+//     console.log("we just saved a cat to the DB");
+//     console.log(cat);
+//   }
+// });
+
+//another way:
+//make a new one and save to db
+Cat.create({
+  name: "snow white",
+  age: 15,
+  temperment: "bland"
+}, function(err, cat){
+  if(err){
+    console.log(err);
   } else {
-    console.log("we just saved a cat to the DB");
     console.log(cat);
   }
 });
+
+
+
+
+
 
 //open the terminal and type mongo
   //show dbs
