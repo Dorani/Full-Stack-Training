8e42dea936f3089e573713b0cd7581474aef6552
Seif <seif.dorani@gmail.com>
2018-09-24 22:38:25 -0700

    create a state

--- Front-End/projectmanager/src/App.js
+++ Front-End/projectmanager/src/App.js
@@ -3,6 +3,28 @@ import Projects from './components/projects'
 import './App.css';
 
 class App extends Component {
+  //all our data goes into state
+  construtor(){
+    this.state = {
+      projects: [
+        {
+          title: "B to B Engineer",
+          category: "Web Design"
+        },
+
+        {
+          title: "Social App",
+          category: "Mobile"
+        },
+
+        {
+          title: "Ecommerce",
+          category: "Back End Engineer"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
