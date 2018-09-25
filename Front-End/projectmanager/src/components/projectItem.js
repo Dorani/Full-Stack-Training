80783c110a7029d9a4feee661ccb60a35781ffb4
Seif <seif.dorani@gmail.com>
2018-09-25 09:22:48 -0700

    data stores, next render component with data

---
+++ Front-End/projectmanager/src/components/projectItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <li className="Project">
+        
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
--- Front-End/projectmanager/src/components/projects.js
+++ Front-End/projectmanager/src/components/projects.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+
   render() {
-    console.log(this.props);
+    let projectItems;
+    if (this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        console.log(project);
+      });
+    }
     return (
       <div className="Projects">
         <h1>Welcome to Project Management</h1>
