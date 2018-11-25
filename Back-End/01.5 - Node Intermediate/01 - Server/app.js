cf5bd619ec9b366ed20f8d281768db25fdc7e19f
Seif <seif.dorani@gmail.com>
2018-11-25 15:31:39 -0800

    created node server using es6 and http core module

---
+++ Back-End/01.5 - Node Intermediate/01 - Server/app.js
@@ -0,0 +1,13 @@
+//require module http
+const http = require('http');
+
+//event driven architecture
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+
+//listen for incoming request on port 3000
+server.listen(3000, () => {
+  console.log("we live!")
+});
