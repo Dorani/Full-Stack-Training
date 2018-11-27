8ec4e874d6f5b83c98be9858ee60d2c7f4aa3a5f
Seif <seif.dorani@gmail.com>
2018-11-27 09:27:12 -0800

    required file, used routes in server creating

--- Back-End/01.5 - Node Intermediate/01 - Server/app.js
+++ Back-End/01.5 - Node Intermediate/01 - Server/app.js
@@ -1,11 +1,9 @@
 //require the http module
 const http = require('http');
-
+const routes = require('./routes')
 
 //event driven architecture
-const server = http.createServer((req, res) => {
-  
-});
+const server = http.createServer(routes);
 
 
 //listen for incoming request on port 3000
