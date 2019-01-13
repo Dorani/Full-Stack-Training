7c245f9e9ea737aa7fca5d274d860265b3fd9249
Seif <seif.dorani@gmail.com>
2019-01-12 23:01:31 -0800

    created react componenent unsing jsx and es6 arrow func

--- Front-End/06-React/01-First_App/jsx/src/index.js
+++ Front-End/06-React/01-First_App/jsx/src/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 //Creacte a react component
-
+const app = () => {
+  return <div>Hello</div>
+}
 
 //Take the react componenent and show it on the browser
