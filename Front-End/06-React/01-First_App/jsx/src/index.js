e8d0a1a88af3e4c24212a7dd0f825cac598ca85a
Seif <seif.dorani@gmail.com>
2019-01-12 23:05:31 -0800

    render component into the main index file where there is a div with the id of root, this is where all of our react components will live

--- Front-End/06-React/01-First_App/jsx/src/index.js
+++ Front-End/06-React/01-First_App/jsx/src/index.js
@@ -3,8 +3,12 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 //Creacte a react component
-const app = () => {
+const App = () => {
   return <div>Hello</div>
-}
+};
 
 //Take the react componenent and show it on the browser
+ReactDom.render(
+  <App />,
+  document.querySelector('#root')
+);
