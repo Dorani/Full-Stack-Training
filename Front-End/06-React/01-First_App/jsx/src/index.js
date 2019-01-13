c251ad885c39ef1ba324f23e8a6e6ca25bdc777c
Seif <seif.dorani@gmail.com>
2019-01-12 22:49:37 -0800

    breaking down intro with importanting libraries, reactdom and react

---
+++ Front-End/06-React/01-First_App/jsx/src/index.js
@@ -0,0 +1,8 @@
+//Import the React and ReactDom libraries
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//Creacte a react component
+
+
+//Take the react componenent and show it on the browser
