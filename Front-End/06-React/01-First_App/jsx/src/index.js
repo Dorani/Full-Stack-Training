866fd4044cdb1f6579af25d90f26b95963ceb8b5
Seif <seif.dorani@gmail.com>
2019-01-13 23:19:56 -0800

    edit jsx lingo to avoid colisions with this keyword of class, cause in js thats a keyword

--- Front-End/06-React/01-First_App/jsx/src/index.js
+++ Front-End/06-React/01-First_App/jsx/src/index.js
@@ -6,7 +6,7 @@ import ReactDom from 'react-dom'
 const App = () => {
   return (
     <div>
-      <label class="label" for="name">Enter Name:</label>
+      <label className="label" for="name">Enter Name:</label>
       <input id="name" type="text"/>
       <button style={{backgroundColor: 'blue', color: 'white'}}>
       Submit
