5da82ae3072fe04167a7013332f3e09727420fc5
Seif <seif.dorani@gmail.com>
2019-01-16 09:57:42 -0800

    added simple div with text, works

---
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return <div>Hi There</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
