6593457f84adf2b0d5168b2a21c38b0b933b2308
Seif <seif.dorani@gmail.com>
2019-01-16 23:22:43 -0800

    so this is what you are NOT SUPPOSED TO DO

--- Front-End/06-React/02-Components/components/src/index.js
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -19,6 +19,36 @@ const App = () => {
           <div className="text">Nice blog post!</div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Todat at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Todat at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
   );
 };
