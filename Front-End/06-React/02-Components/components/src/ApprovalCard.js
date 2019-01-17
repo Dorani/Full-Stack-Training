a192e2998e07c11246f32d635f53c2a4bdc1eb61
Seif <seif.dorani@gmail.com>
2019-01-17 09:30:50 -0800

    now we pass in props.children and voila we got a component within a component

--- Front-End/06-React/02-Components/components/src/ApprovalCard.js
+++ Front-End/06-React/02-Components/components/src/ApprovalCard.js
@@ -6,7 +6,7 @@ const ApprovalCard = (props) => {
   console.log(props.children)
   return (
     <div className="ui card">
-      <div className="content"> Are you sure? </div>
+      <div className="content"> {props.children} </div>
       <div className="extra content">
         <div className="ui two buttons">
           <div className="ui basic green button">Approve</div>
