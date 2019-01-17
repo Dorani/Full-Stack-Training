b47317608ec2e490d8c412be9bf65e2a5fd9b887
Seif <seif.dorani@gmail.com>
2019-01-16 22:50:21 -0800

    create wall post component

--- Front-End/06-React/02-Components/components/src/index.js
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
-  return <div>Hi There</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Todat at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
