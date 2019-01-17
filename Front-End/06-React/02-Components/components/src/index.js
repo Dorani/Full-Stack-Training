31b650e81ff981e1dd3bfc547f260738b2b1bf3b
Seif <seif.dorani@gmail.com>
2019-01-17 09:20:51 -0800

    approval card added

--- Front-End/06-React/02-Components/components/src/index.js
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
-
-
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="sam"
         timeAgo="Today at 4:45PM"
