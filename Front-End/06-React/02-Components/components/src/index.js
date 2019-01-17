975f632a120ff24bf29b501797e6b4a4b46fc808
Seif <seif.dorani@gmail.com>
2019-01-16 23:16:09 -0800

    faker lib important, then we use double curlies to add thee faker obj with the nested properties that we can seee from the docs

--- Front-End/06-React/02-Components/components/src/index.js
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
