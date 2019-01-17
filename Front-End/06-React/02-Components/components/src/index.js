9f5b4eae6e95915fb762e4c47f0f95372a584037
Seif <seif.dorani@gmail.com>
2019-01-17 09:40:54 -0800

    adding stuff

--- Front-End/06-React/02-Components/components/src/index.js
+++ Front-End/06-React/02-Components/components/src/index.js
@@ -9,6 +9,14 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+
+      <ApprovalCard>
         <CommentDetail
           author="sam"
           timeAgo="Today at 4:45PM"
