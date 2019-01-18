0aea3df8a24fb9f311a1154d84a295710c55e947
Seif <seif.dorani@gmail.com>
2019-01-18 09:35:37 -0800

    able to get current coordinates logged using built in browser functions for geotracking

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -3,6 +3,13 @@ import ReactDom from 'react-dom';
 
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    //if an error occurs with above we will log it
+    (error) => console.log(error),
+    //callback that gets called when above goes as planned
+    (position) => console.log(position)
+  );
+
   return <div>Hi there!</div>
 };
 
