c4c91616f0db03d2e1a2b2719b09352503fc4225
Seif <seif.dorani@gmail.com>
2019-01-18 09:47:34 -0800

    refactored

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -14,4 +14,19 @@ const App = () => {
 };
 
 
+//refactored to class based componenet
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      //if an error occurs with above we will log it
+      (error) => console.log(error),
+      //callback that gets called when above goes as planned
+      (position) => console.log(position)
+    );
+    
+    return <div>Latitude: </div>
+  }
+}
+
+
 ReactDom.render(<App />, document.querySelector("#root"));
