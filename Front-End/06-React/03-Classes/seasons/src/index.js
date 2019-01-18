568440a1a610630621d2b1885eb82ba14101e6b6
Seif <seif.dorani@gmail.com>
2019-01-18 09:48:46 -0800

    removed functional and subclassing react.component so we can borrow functionality into our class

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    //if an error occurs with above we will log it
-    (error) => console.log(error),
-    //callback that gets called when above goes as planned
-    (position) => console.log(position)
-  );
-
-  return <div>Latitude: </div>
-};
-
-
 //refactored to class based componenet
 class App extends React.Component {
   render() {
@@ -23,7 +10,7 @@ class App extends React.Component {
       //callback that gets called when above goes as planned
       (position) => console.log(position)
     );
-    
+
     return <div>Latitude: </div>
   }
 }
