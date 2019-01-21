a6348634bed7375a5c1deea1d6f9c75e742ec86f
Seif <seif.dorani@gmail.com>
2019-01-21 15:47:17 -0800

    refactored render to return jsx depending on the state of our prop

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -30,13 +30,15 @@ class App extends React.Component {
   }
   //React says we have to define render!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-  );
+    if (this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Lattitude: {this.state.lat} </div>
+    }
+
+    return <div>Loading!</div>
   }
 }
 
