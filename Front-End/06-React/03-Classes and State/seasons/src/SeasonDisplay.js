a9f4d4b3a5333204c9ab7aa3a913789a00bc2161
Seif <seif.dorani@gmail.com>
2019-01-21 15:52:11 -0800

    renamed direct and added diagrams for state etc

---
+++ Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return <div>Ok NOW!</div>
+};
+
+export default SeasonDisplay;
---
+++ Front-End/06-React/03-Classes and State/seasons/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//Refactored to class based componenet
+//Our app component is extending, or borrowing functionality
+//from the React component base class, it has a constructor function
+//of its own that has some code to set up react app for us
+//when we create we constructor function, we override react.componenent
+// to fix this we call super(), which is a reference to the parents constuctor
+class App extends React.Component {
+  constructor(props) {
+    //pointer to the React.Component constructor
+    super(props);
+    //state object that will eventually contain data
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      //callback that gets called when above goes as planned
+      (position) => {
+        console.log(position)
+        //take Latitude to update state object
+        //don't forget set state!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      //if an error occurs with above we will log it
+      (error) => {
+        this.setState({errorMessage: error.message});
+      }
+    );
+  }
+  //React says we have to define render!\
+  //3 diff combo of state:
+    //lat and no error msg = show lat
+    //no lat and have error = show error
+    //no lat and no errorMessage = show "loading"
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Lattitude: {this.state.lat} </div>
+    }
+
+    return <div>Loading!</div>
+  }
+}
+
+ReactDom.render(<App />, document.querySelector("#root"));
