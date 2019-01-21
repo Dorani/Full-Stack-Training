3a075366014ba3cb30ec26cfb32ccc08c6a97fe4
Seif <seif.dorani@gmail.com>
2019-01-20 23:00:44 -0800

    val of nul we dont know what it is yet;

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     //pointer to the React.Component constructor
     super(props);
     //state object that will eventually contain data
-    this.state = { lat: };
+    this.state = { lat: null };
   }
 
   //React says we have to define render!
