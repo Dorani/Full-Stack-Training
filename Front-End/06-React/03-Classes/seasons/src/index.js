c966fe04f7fc3a32824afabdd03b93c46e0f483a
Seif <seif.dorani@gmail.com>
2019-01-20 22:57:55 -0800

    notes about super in react

--- Front-End/06-React/03-Classes/seasons/src/index.js
+++ Front-End/06-React/03-Classes/seasons/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//refactored to class based componenet
+//Refactored to class based componenet
+//Our app component is extending, or borrowing functionality
+//from the React component base class, it has a constructor function
+//of its own that has some code to set up react app for us
+//when we create we constructor function, we override react.componenent
+// to fix this we call super(), which is a reference to the parents constuctor
 class App extends React.Component {
+
+  constructor(props) {
+    //pointer to the React.Component constructor
+    super(props);
+  }
+
+  //React says we have to define render!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       //if an error occurs with above we will log it
