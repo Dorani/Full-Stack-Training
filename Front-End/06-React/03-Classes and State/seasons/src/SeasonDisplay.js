f2b0911f13355c3887e66b993ab98b5aec72a1c2
Seif <seif.dorani@gmail.com>
2019-01-22 22:18:52 -0800

    returning season based of geolcation, working

--- Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
+++ Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 //determine what season is function
 const getSeason = (lat, month) => {
-  if (month > 2 and month < 9) {
+  if (month > 2 && month < 9) {
     //if lat is > 0 returns true then reuturn summer, otherwise winter
     //ternary expressions:
     return lat > 0 ? 'summer' : 'winter';
   } else {
-    return lat < 0 ? 'wintr' : 'summer';
+    return lat > 0 ? 'winter' : 'summer';
   }
 }
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(props.lat);
+  console.log(season);
   return <div>Season Display</div>
 };
 
