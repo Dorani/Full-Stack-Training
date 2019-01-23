156c547590d745a73f333a610699f5225ef9b552
Seif <seif.dorani@gmail.com>
2019-01-22 22:15:07 -0800

    conditionals with ternary expressions

--- Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
+++ Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
+//determine what season is function
+const getSeason = (lat, month) => {
+  if (month > 2 and month < 9) {
+    //if lat is > 0 returns true then reuturn summer, otherwise winter
+    //ternary expressions:
+    lat > 0 ? 'summer' : 'winter';
+  }
+}
+
 const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
   console.log(props.lat);
   return <div>Season Display</div>
 };
