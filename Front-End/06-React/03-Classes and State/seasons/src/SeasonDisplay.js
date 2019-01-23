8211f46ec8cb2993e9ec9929188f8806853ba500
Seif <seif.dorani@gmail.com>
2019-01-22 22:45:02 -0800

    plainly spelled out

--- Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
+++ Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
@@ -25,9 +25,7 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-  season === 'winter' ? 'burr, it is chilly' : 'lets hit the beach';
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  seasonConfig[season] //{text, iconName}
   return (
     <div>
       <i className={`${icon} icon`} />
