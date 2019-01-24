bc4d7edc69b1ec7b2cdd1e3e87f1f443581761a7
Seif <seif.dorani@gmail.com>
2019-01-23 23:12:35 -0800

    reusable spinner animation componenet for when pages load for ui expreeriencee

---
+++ Front-End/06-React/03-Classes and State/seasons/src/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Spinner = () => {
+  return (
+    <div className="ui activee dimmer">
+      <div className="ui big text loader">
+        Loading...
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
