b59b6aa4b7fcc92c7533d74a5c501c0353b88bf4
Seif <seif.dorani@gmail.com>
2019-01-23 21:31:56 -0800

    styless

--- Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
+++ Front-End/06-React/03-Classes and State/seasons/src/SeasonDisplay.js
@@ -31,7 +31,7 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season]; //{text, iconName}
   return (
     <div>
-      <i className={` icon-left massive ${iconName} icon`} />
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
       <i className={`icon-right massive ${iconName} icon`} />
     </div>
