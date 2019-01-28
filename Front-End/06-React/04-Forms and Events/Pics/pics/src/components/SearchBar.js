de594b21a8df32f526ea646794f20f7587645085
Seif <seif.dorani@gmail.com>
2019-01-28 15:36:01 -0800

    passing a callback to a event handler, user types text, thennnn we fire oninputchange

--- Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
+++ Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
@@ -3,13 +3,19 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+
+  onInputChange() {
+
+  }
+
+
   render() {
     return (
     <div className="ui segment">
       <form className="ui form">
         <div className="field">
         <label>Image Search</label>
-        <input type="text" />
+        <input type="text" onChange={this.onInputChange} />
         </div>
       </form>
     </div>
