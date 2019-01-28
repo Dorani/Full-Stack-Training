679ea28dfdd6c7e09ebb61b6bf35765ca41914de
Seif <seif.dorani@gmail.com>
2019-01-28 15:53:52 -0800

    refactored code more eff

--- Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
+++ Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
@@ -3,20 +3,20 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
-  //handler with event object
-  onInputChange(event) {
-    //containts the text the user types in
-    console.log(event.target.value);
-  }
 
+  state = { term: ''}
   render() {
     return (
     <div className="ui segment">
       <form className="ui form">
         <div className="field">
         <label>Image Search</label>
-        //prop named onChange is used for wiring up callback
-        <input type="text" onChange={this.onInputChange} />
+    
+        <input
+          type="text"
+          value={this.state.term}
+          onChange={(e) => this.setState({term: e.target.value})}
+        />
         </div>
       </form>
     </div>
