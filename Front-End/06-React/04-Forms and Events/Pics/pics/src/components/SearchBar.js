e4941cdddb83ecf3a2d2641943030930db2a5487
Seif <seif.dorani@gmail.com>
2019-01-29 10:16:40 -0800

    event prevent default form on submit handler

--- Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
+++ Front-End/06-React/04-Forms and Events/Pics/pics/src/components/SearchBar.js
@@ -5,10 +5,14 @@ import React from 'react';
 class SearchBar extends React.Component {
 
   state = { term: ''}
+  //prevent form from submitting itself
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
   render() {
     return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={this.onFormSubmit} className="ui form">
         <div className="field">
         <label>Image Search</label>
 
