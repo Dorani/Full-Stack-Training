e078f5860a2cc9e32a299f732d86320404d75e1e
Seif <seif.dorani@gmail.com>
2019-02-11 22:02:27 -0800

    fixed typos

--- Front-End/06-React/04-Forms and Events/Pics/pics/src/components/App.js
+++ Front-End/06-React/04-Forms and Events/Pics/pics/src/components/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar'
 
 
-class App extends React.Component = () => {
+class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar />
       </div>
-      )
-    };
+      );
+    }
   }
 export default App;
