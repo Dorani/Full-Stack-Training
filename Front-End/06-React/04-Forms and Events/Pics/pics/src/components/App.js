bfdd529ec7de115ae311ac80ad5660e1006aeb8e
Seif <seif.dorani@gmail.com>
2019-02-11 22:00:34 -0800

    refactor a function component to class based

--- Front-End/06-React/04-Forms and Events/Pics/pics/src/components/App.js
+++ Front-End/06-React/04-Forms and Events/Pics/pics/src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar'
 
 
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  )
-};
-
+class App extends React.Component = () => {
+  render() {
+    return (
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar />
+      </div>
+      )
+    };
+  }
 export default App;
